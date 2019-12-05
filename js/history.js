fabc3cd3b1ecf64e55da94e03e9cd370d0cd3170
vInceptive <mistervq@gmail.com>
2019-12-04 21:05:22 -0800

    Mostly finished.
    Submitable history page.

--- js/history.js
+++ js/history.js
@@ -3,15 +3,15 @@
 
 
 
-var word1 = 'Good4you',
-    word2 = 'IamGreat';
+var word1 = 'help.',
+    word2 = 'HAHA!';
     state = true;
 
-$('p').html(function(_, html) {
+$('h3').html(function(_, html) {
     return '<span>' + html.split('').join('</span><span>') + '</span>';
 });
 
-$('p').on({
+$('h3').on({
     mouseenter: function() {
         $('span', this).each(function(i, item) {
             $(item).delay(i*300).fadeOut(100, function() {
